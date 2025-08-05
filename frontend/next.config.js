54bff7fa61dd97044f0703a1e326e69622eddefd
vipbhardwaj <vipul.bhardwaj1011@gmail.com>
2025-08-05 22:57:44 +0530

    alias resolution fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
 
-module.exports = nextConfig 
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
